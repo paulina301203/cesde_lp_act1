cc73241816acf26c91b2dad488228162cf4a6bdf
paulina301203 <paulinacelular3012@gmail.com>
2026-05-17 23:05:02 +0000

    ejercicio4

--- src/main/java/com/example/Operadores.java
+++ src/main/java/com/example/Operadores.java
@@ -2,19 +2,29 @@ package com.example;
 
 public class Operadores {
     public static void demostrarAritmeticos() {
-        // TODO
+        float nota1 = 3.5f;
+        float nota2 = 4.0f;
+        float nota3 = 3.7f;
+        float promedio = (nota1 + nota2 +nota3)/3f;
     }
 
     public static void demostrarIncrementoDecremento() {
-        // TODO
+        int n = 5;
+        System.out.println("Usando ++n: " + (++n));
+        System.out.println("Usando n++: " + (n++));
     }
 
     public static void demostrarRelacionales() {
-        // TODO
+        
     }
 
     public static void demostrarLogicos() {
-        // TODO
+        int x = 40;
+        if (x > 10 && x < 50) {
+            System.out.println("True: Es mayor que 10 y menor que 50");
+        }else{
+            System.out.println("False: No es mayor que 10 y menor que 50");
+        }
     }
 
     public static void demostrarAsignacionCompuesta() {
@@ -22,6 +32,8 @@ public class Operadores {
     }
 
     public static void demostrarOperadorTernario() {
-        // TODO
+        int numero1 = 4;
+        String mensaje = (numero1<0) ? "Es negativo" : "Es positivo";
+        System.out.println(mensaje);
     }
 }
