94290f97f6ce0fb27250c7a664efbadb48a37d7b
paulina301203 <paulinacelular3012@gmail.com>
2026-05-18 19:49:58 +0000

    ejercicio6 taller

--- src/main/java/com/example/EstructurasCondicionales.java
+++ src/main/java/com/example/EstructurasCondicionales.java
@@ -2,11 +2,19 @@ package com.example;
 
 public class EstructurasCondicionales {
     public static void ejemploIf() {
-        // TODO
+        int edad = 16;
+        if (edad < 18) {
+            System.out.println("No es apto para votar");
+        }
     }
 
     public static void ejemploIfElse() {
-        // TODO
+        int edad = 16;
+        if (edad < 18) {
+            System.out.println("No es apto para votar");
+        }else{
+            System.out.println("Es apto para votar");
+        }
     }
 
     public static void ejemploIfElseIfElse() {
@@ -14,10 +22,52 @@ public class EstructurasCondicionales {
     }
 
     public static void ejemploSwitch() {
-        // TODO
+        int dia = 3;
+        String nombreDia;
+        switch (dia) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+
+            case 2:
+                System.out.println("Martes");
+                break;
+
+            case 3:
+                System.out.println("Miercoles");
+                break;   
+            
+            case 4:
+                System.out.println("Jueves");
+                break;  
+                
+            case 5:
+                System.out.println("viernes");
+                break;   
+                
+            case 6:
+                System.out.println("Sabado");
+                break; 
+                
+            case 7:
+                System.out.println("Domingo");
+                break;     
+        
+            default:
+                System.out.println("Novalido");
+                break;
+        }
     }
 
     public static void ejemploSwitchExpression() {
-        // TODO
+       String estacion = "Verano";
+       String clima = switch (estacion) {
+        case "Verano"    -> "Hace mucho calor y sol";
+        case "Invierno"  -> "Hace mucho frío y llueve";
+        case "Primavera" -> "Las flores florecen y el clima es templado";
+        case "Otoño"     -> "Las hojas caen y hace viento";
+        default          -> "Estación desconocida"; 
+         };
+         System.out.println("En " + estacion + ": " + clima);
     }
 }
--- src/main/java/com/example/EstructurasDeRepeticion.java
+++ src/main/java/com/example/EstructurasDeRepeticion.java
@@ -1,12 +1,25 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class EstructurasDeRepeticion {
     public static void ejemploWhile() {
-        // TODO
+        int contador = 10;
+        while (contador >=1) {
+            System.out.println(contador);
+            contador--;
+        }
     }
 
     public static void ejemploDoWhile() {
-        // TODO
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        do {
+            System.out.println("Ingresa un numero");
+            numero = teclado.nextInt();
+        } while (numero != 0);
+
+        System.out.println("Fin del programa");
     }
 
     public static void ejemploForClasico() {
@@ -14,11 +27,24 @@ public class EstructurasDeRepeticion {
     }
 
     public static void ejemploForAnidado() {
-        // TODO
+        for (int i = 1; i <= 10; i++){
+            System.out.println("Tabla del " + int);
+
+            for(int j = 1; j <= 10; j++){
+                int resultado = i * j;
+                System.out.println(i + " x " + j + " = " + resultado);
+            }
+        }
     }
 
     public static void ejemploBreakContinue() {
-        // TODO
+        for (int i = 1; i<= 10; i++){
+            if (i==7) {
+                System.out.println("se encontro el 7, se termina el bucle");
+                break;
+            }
+            System.out.println("el numero actual es: " + i);
+        }
     }
 
     public static void ejemploEtiquetas() {
